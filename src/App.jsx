f93f95688da44c471652c5fde3a91f9189e008bb
harshScript <harshpal022@gmail.com>
2023-08-06 01:03:00 +0530

    uploading source code of real-estate-ui-using-react

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero';
+import Companies from './components/Companies/Companies';
+import './App.css'
+import Residencies from './components/Residencies/Residencies';
+import Value from './components/Value/Value';
+import Contact from './components/Contact/Contact';
+import GetStarted from './components/GetStarted/GetStarted';
+import Footer from './components/Footer/Footer';
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient"></div>
+        <Header></Header>
+        <Hero></Hero>
+      </div>
+      <Companies></Companies>
+      <Residencies/>
+      <Value/>
+      <Contact></Contact>
+      <GetStarted></GetStarted>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
